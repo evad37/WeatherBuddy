c4cd57c15a03c953b0a5db6134ccff75823b6c0a
David Stern <evad37@gmail.com>
2020-10-20 11:08:35 +0800

    Parse api data for location model's GetWeather method

--- WeatherBuddy/WeatherBuddy/Models/Location.cs
+++ WeatherBuddy/WeatherBuddy/Models/Location.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace WeatherBuddy.Models
 {
@@ -12,6 +16,10 @@ namespace WeatherBuddy.Models
         string country;
         double tempNow;
         string conditions;
+        /// <summary>
+        /// Tuple of error message headring and body
+        /// </summary>
+        (string, string) errorMessage = ("", "");
 
         public string stateAndCountry
         {
@@ -20,10 +28,45 @@ namespace WeatherBuddy.Models
                 : string.Format("{0}, {1}", state, country);
         }
 
-        public async void GetWeather(Api api)
+        public async Task<bool> GetWeather(Api api)
         {
-            string jsonResponse = await api.FetchData(id.ToString());
-            // TODO: parse json response to get temp and conditions
+            bool isLoaded = false;
+            try
+            {
+                // Fetch info from api
+                string responseString = await api.FetchData(id.ToString());
+                // Parse relevant data out of the json response string
+                JObject response = JsonConvert.DeserializeObject<JObject>(responseString);
+                JObject responseMain = response.Value<JObject>("main");
+                tempNow = responseMain.Value<double>("temp");
+                JArray responseWeatherArray = response.Value<JArray>("weather");
+                JObject responseWeather = (JObject)responseWeatherArray.First;
+                conditions = responseWeather.Value<string>("description");
+                errorMessage = ("", "");
+                isLoaded = true;
+            }
+            catch (NoInternetException)
+            {
+                errorMessage = ("No internet connection", "Please connect to the internet and try again.");
+            }
+            catch (HttpRequestException) // Error in internet connection, or in api url
+            {
+                errorMessage = ("Connection error", "Conversion rates could not be retrieved.");
+            }
+            catch (BadResponseException e) // Error from api returning a bad response 
+            {
+                errorMessage = ("Server error", e.Message);
+            }
+            catch (FormatException e) // Error in parsing data
+            {
+                errorMessage = ("Data error", e.Message);
+            }
+            catch (Exception e) // Any other errors
+            {
+                errorMessage = ("Error", e.Message);
+            }
+
+            return isLoaded;
         }
     }
 }
